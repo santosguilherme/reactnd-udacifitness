ab1b34cb5f2359f139480bb3d790accd2b5d5c5b
Guilherme Santos <sntguilherme@gmail.com>
2018-01-13 11:52:02 -0200

    actions and reducers

--- App.js
+++ App.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 import {View} from 'react-native';
 
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 import AddEntry from './components/AddEntry';
+
+import reducers from './reducers';
 
 
 export default class App extends React.Component {
     render() {
         return (
-            <View>
-                <AddEntry/>
-            </View>
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <AddEntry/>
+                </View>
+            </Provider>
         );
     }
 }
